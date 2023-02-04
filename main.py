8d72238765b1815a4aa85bb066649db9ff18cc00
Doris <doris.gohawks@gmail.com>
2023-02-03 20:04:30 -0500

    main change

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from cmu_graphics import *
+
+from cryLog import *
+from frontPage import *
+from myStory import *
+from settings import *
+from splashScreen import *
+
+def main():
+    runAppWithScreens(initialScreen='splashScreen', width= 390, length = 844)
+main()
