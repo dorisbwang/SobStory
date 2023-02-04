6b95556337e8e105b235fe22d945a758eeba497f
spavlinek <sapavlinek@gmail.com>
2023-02-04 16:21:41 -0500

    dataAnalysis more

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -4,7 +4,12 @@ monthlyCryLog = {"Jan": 0, "Feb": 0, "Mar": 0, "Apr": 0, "May": 0,
                 "Nov": 0, "Dec": 0 }
 reasonLog = dict()
 
-reasons = {"love": "heartbreak, boyfriend, girlfriend, love, kiss, date, breakup, divorce, "
+reasonsKeyWords = {"relationship": ["relationship", "heartbreak", "boyfriend", "girlfriend", "love", "kiss", "date", "breakup", "divorce"],
+            "school": ["math", "class", "school", "grades", "exams", "concepts", "computer science"],
+            "family": ["mom", "dad", "brother", "sister", "family", "house", "grandma", "grandpa"],
+            "money" : ["payment", "money", "dollars", "debt", "broke"],
+            "health" : ["cancer", "health", "sick", "terminal", "death depression"],
+            "work" : ["boss", "job", "coworker", "businness","work"],
                     }
 
 def dateAnalysis(dateList):
@@ -18,5 +23,9 @@ def dateAnalysis(dateList):
                 monthlyCryLog[m] = monthlyCryLog[m] + 1
             i += 1
 
-def reasonAnalysis(reasons):
-    pass
+def reasonAnalysis(log):
+    for reason in reasonsKeyWords:
+        for keyword in reasonsKeyWords[reason]:
+            if keyword in log:
+                    reasonLog[reason] = reasonLog.get(reason, 0) +  1
+                    
